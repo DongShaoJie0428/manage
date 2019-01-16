960601a3b643ebfd98b4a33848d3d6af40aa8caf
dongshaojie <M13520100667@163.com>
2019-01-16 15:25:42 +0800

    前端修改权限

--- src/views/userlist/index.vue
+++ src/views/userlist/index.vue
@@ -30,31 +30,60 @@
           <span style="margin-left: 10px">{{ scope.row.email }}</span>
         </template>
       </el-table-column>
+      <el-table-column label="角色" width="150">
+        <template slot-scope="scope">
+          <el-tag v-for="tag in scope.row.rolers" :key="tag" style="margin-left:3px;">
+            {{ tag }}
+          </el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column label="权限管理" width="160">
+        <template slot-scope="scope">
+          <el-tag v-for="tag in scope.row.access" :key="tag" style="margin-left:3px;">
+            {{ tag }}
+          </el-tag>
+        </template>
+      </el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
+          <el-button size="mini" @click="handleRolers(scope.$index, scope.row)">修改权限</el-button>
           <el-button size="mini" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
           <el-button size="mini" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
         </template>
       </el-table-column>
     </el-table>
 
-    <el-dialog :visible.sync="dialogVisible" :before-close="handleClose" title="提示" width="80%">
+    <el-dialog :visible.sync="dialogVisible" :before-close="handleClose" :title="type==='edit'?'编辑用户信息':'用户修改角色'" width="80%">
 
       <el-form ref="form" :model="currentList" :rules="userRules" label-position="right" label-width="80px">
-        <el-form-item label="姓名" prop="username">
+        <!-- 修改用户信息的 -->
+        <el-form-item v-if="type=='edit'" label="姓名" prop="username">
           <el-input v-model="currentList.username"/>
         </el-form-item>
-        <el-form-item label="头像">
+        <el-form-item v-if="type=='edit'" label="头像">
           <el-upload :show-file-list="false" class="avatar-uploader" action="123">
             <img v-if="currentList.avatar" :src="currentList.avatar" class="avatar" style="width:100px">
             <i v-else class="el-icon-plus avatar-uploader-icon"/>
           </el-upload>
         </el-form-item>
-        <el-form-item label="手机号" prop="phone">
+        <el-form-item v-if="type=='edit'" label="手机号" prop="phone">
           <el-input v-model="currentList.phone"/>
         </el-form-item>
-        <el-form-item label="邮箱" prop="email">
+        <el-form-item v-if="type=='edit'" label="邮箱" prop="email">
           <el-input v-model="currentList.email"/>
+        </el-form-item>
+        <!-- 修改用户角色的 -->
+        <el-form-item v-if="type=='roler'" label="我的角色">
+          <el-tag v-for="tag in myRolers" :key="tag" style="margin-left:5px" closable @close="handleCloseRoler(tag)">
+            {{ tag }}
+          </el-tag>
+        </el-form-item>
+        <el-form-item v-if="type=='roler'" label="权限">
+          <el-tag v-for="tag in allRolers" :key="tag" style="margin-left:5px">
+            <span @click="handleAddRoler(tag)">
+              {{ tag }}
+            </span>
+          </el-tag>
         </el-form-item>
       </el-form>
 
@@ -89,7 +118,10 @@ export default {
     return {
       current: 1,
       dialogVisible: false,
+      type: '', // 弹框的类型，如果是edit就是用户编辑信息，roler就是修改角色
       currentList: {},
+      myRolers: [],
+      allRolers: ['boss', 'developer', 'producter', 'operator', 'designer'],
       userRules: {
         username: [{ required: true, trigger: 'blur', message: '请填写姓名' }],
         phone: [{ required: true, trigger: 'blur', validator: phoneValidator }],
@@ -111,9 +143,29 @@ export default {
       updataList: 'userlist/updataList',
       deleteUser: 'userlist/deleteUser'
     }),
+    // 点击添加上面的角色
+    handleAddRoler(tag) {
+      console.log(tag)
+      this.myRolers.push(tag)
+      this.myRolers = [...new Set(this.myRolers)]
+    },
+    // 点击删除角色
+    handleCloseRoler(tag) {
+      const index = this.myRolers.findIndex(item => item === tag)
+      this.myRolers.splice(index, 1)
+    },
+    // 点击修改权限
+    handleRolers(index, row) {
+      this.type = 'roler'
+      this.dialogVisible = true
+      this.currentList = { ...row }
+      this.myRolers = [...row.rolers]
+    },
+    // 点击编辑信息
     handleEdit(index, row) {
       // console.log(index, row)
       this.dialogVisible = true
+      this.type = 'edit'
       this.currentList = { ...row }
     },
     // 点击删除，删除用户信息
